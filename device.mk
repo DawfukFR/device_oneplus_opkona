1b3deca5bf852e5bdd434cbcf05f4780471810e1
markakash <akashniki@gmail.com>
2022-12-20 00:16:14 +0530

    opkona: Switch to AIDL WiFi HAL

--- device.mk
+++ device.mk
@@ -603,7 +603,8 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    android.hardware.wifi.hostapd@1.0.vendor \
+    android.hardware.wifi.supplicant \
+    android.hardware.wifi.hostapd \
     hostapd \
     wpa_supplicant
 
